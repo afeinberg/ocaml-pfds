52d8520f68db4c2cad36d457d4b464ae77883c14
Alex Feinberg <alex@strlen.net>
2011-02-01 03:37:08 -0800

    Make test_heap purely polymoprhic

--- lib_test/TestHeaps.ml
+++ lib_test/TestHeaps.ml
@@ -6,20 +6,20 @@ module OrderedInt  = struct
   let compare = compare
 end
 
-let test_heap impl =
-  let module H = (val impl: HeapSig with type elt = int) in
+let test_heap (type elt) impl vals =
+  let module H = (val impl: HeapSig with type elt = elt) in
   let h = H.empty in
-  let h1 = H.add 1 h in
-  let h2 = H.add 2 h1 in
+  let h1 = H.add vals.(0) h in
+  let h2 = H.add vals.(1) h1 in
   let h3 = H.take_min h2 in
 
-  assert_equal ~msg:"add works" (H.peek_min h1) (Some 1) ;
-  assert_equal ~msg:"peek_min returns min" (H.peek_min h2) (Some 1) ;
-  assert_equal ~msg:"take_min removes min" (H.peek_min h3) (Some 2)
+  assert_equal ~msg:"add works" (H.peek_min h1) (Some vals.(0)) ;
+  assert_equal ~msg:"peek_min returns min" (H.peek_min h2) (Some vals.(0)) ;
+  assert_equal ~msg:"take_min removes min" (H.peek_min h3) (Some vals.(1))
 
 let test_leftist_heap _ = 
   let module IntLeftistHeap = LeftistHeap (OrderedInt) in
-  test_heap (module IntLeftistHeap : HeapSig with type elt = int)
+  test_heap (module IntLeftistHeap : HeapSig with type elt = int)  [| 1 ; 2 |]
 
 let tests =
   "Heaps" >::: [
